57fa8a4c048ff877281440f3edd696404fb44c4c
wipdev-tech <wipdev.tech@gmail.com>
2023-11-08 10:47:12 +0200

    Added login form

--- main.go
+++ main.go
@@ -7,15 +7,20 @@ import (
 )
 
 func main() {
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-		tmpl.Execute(w, nil)
-	}
-
 	fs := http.FileServer(http.Dir("static"))
-
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", h1)
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/login", handleLogin)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/login.html"))
+	tmpl.Execute(w, nil)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, nil)
 }
