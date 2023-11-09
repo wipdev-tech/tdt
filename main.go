fa3b574dee7cef0ed8696f05b215dce511cc4743
wipdev-tech <wipdev.tech@gmail.com>
2023-11-09 21:31:01 +0200

    Handling POST request and form data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,12 +13,29 @@ func main() {
 	http.HandleFunc("/", handleHome)
 	http.HandleFunc("/login", handleLogin)
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/login.html"))
-	tmpl.Execute(w, nil)
+	switch r.Method {
+	case "GET":
+		tmpl := template.Must(template.ParseFiles("templates/login.html"))
+		tmpl.Execute(w, nil)
+
+    case "POST":
+        err := r.ParseForm()
+        if err != nil {
+            fmt.Println("feeh 7aga msh tamam")
+        }
+
+        fmt.Println(r.PostForm.Get("email"))
+        fmt.Println(r.PostForm.Get("password"))
+
+        http.Redirect(w, r, "/", http.StatusFound)
+        
+    default:
+        fmt.Println("Unsupported method")
+	}
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
