16eaf9753f6fb18c6dc7d74f038017c36632bbf0
wipdev-tech <wipdev.tech@gmail.com>
2023-11-13 20:28:35 +0200

    Added dummy query to test SQLite connection

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wipdev-tech/tdt
 
 go 1.21.1
+
+require github.com/mattn/go-sqlite3 v1.14.18
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+    _ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -13,28 +16,48 @@ func main() {
 	http.HandleFunc("/", handleHome)
 	http.HandleFunc("/login", handleLogin)
 
+    fmt.Println("Listening at 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
+		db, err := sql.Open("sqlite3", "./tdt.db")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		rows, err := db.Query("SELECT * FROM users;")
+		for rows.Next() {
+			var id int
+			var name string
+
+			err = rows.Scan(&id, &name)
+			if err != nil {
+				panic(err)
+			}
+
+            fmt.Println(id, name)
+		}
+
 		tmpl := template.Must(template.ParseFiles("templates/login.html"))
 		tmpl.Execute(w, nil)
 
-    case "POST":
-        err := r.ParseForm()
-        if err != nil {
-            fmt.Println("feeh 7aga msh tamam")
-        }
+	case "POST":
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Println("feeh 7aga msh tamam")
+		}
 
-        fmt.Println(r.PostForm.Get("email"))
-        fmt.Println(r.PostForm.Get("password"))
+		fmt.Println(r.PostForm.Get("email"))
+		fmt.Println(r.PostForm.Get("password"))
 
-        http.Redirect(w, r, "/", http.StatusFound)
-        
-    default:
-        fmt.Println("Unsupported method")
+		http.Redirect(w, r, "/", http.StatusFound)
+
+	default:
+		fmt.Println("Unsupported method")
 	}
 }
 
